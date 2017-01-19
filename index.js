12d2a320a0e1f9be3d57aa0c408364e254ec44b0
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-19 15:44:02 +0100

    Use GraphQL Object Type for Basic Types

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { graphql, buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-type Query {
+type Video {
   id: ID,
   title: String,
   duration: Int,
   watched: Boolean
+}
+type Query {
+  video: Video
 }
 
 type Schema {
@@ -14,19 +17,22 @@ type Schema {
 `);
 
 const resolvers = {
-  id: () => '1',
-  title: () => 'le grand bleu',
-  duration: () => 90,
-  watched: () => true,
-
-}
+  video: () => ({
+    id: () => '1',
+    title: () => 'le grand bleu',
+    duration: () => 90,
+    watched: () => true,
+  }),
+};
 
 const query = `
 query myFirstQuery {
-  id
-  title
-  duration
-  watched
+  video {
+    id,
+    title,
+    duration,
+    watched
+  }
 }
 `;
 
