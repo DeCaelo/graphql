19fae412cb7bbafbe38a556f57e7168d8859be7f
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-22 09:09:03 +0100

    Convert GraphQL List Type to a Relay Connection Type

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const {
  } = require('graphql');
 
  const { getVideoById, getVideos, createVideo } = require('./src/data');
- const { globalIdField } = require('graphql-relay');
+ const {
+  globalIdField,
+  connectionDefinitions,
+  connectionFromPromisedArray,
+  connectionArgs,
+} = require('graphql-relay');
  const { nodeInterface, nodeField } = require('./src/node');
 
 const PORT = process.env.PORT || 3000;
@@ -42,14 +47,31 @@ const videoType = new GraphQLObjectType({
 
 exports.VideoType = videoType;
 
+const { connectionType: VideoConnection } = connectionDefinitions({
+  nodeType: videoType,
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+      description: 'A count of the total number of objects in this connection.',
+      resolve: (conn) => {
+        return conn.edges.length;
+      },
+    },
+  }),
+});
+
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
   description: 'the root query type.',
   fields: {
     node: nodeField,
     videos: {
-      type: new GraphQLList(videoType),
-      resolve: getVideos,
+      type: VideoConnection,
+      args: connectionArgs,
+      resolve: (_,args) => connectionFromPromisedArray(
+        getVideos(),
+        args
+      ),
     },
     video: {
       type: videoType,
